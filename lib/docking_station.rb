921ee273578f586df5b0f160a6f394e0db7b7dfa
Chloe <chloemontgomery01@gmail.com>
2019-01-09 17:42:21 +0000

    Completed 1 - 16

---
+++ lib/docking_station.rb
@@ -0,0 +1,29 @@
+class DockingStation
+  attr_reader :docked_bikes
+  DEFAULT_CAPACITY = 20
+
+  def initialize
+    @docked_bikes = []
+  end
+
+  def release_bike
+    fail 'No bikes available' if empty?
+    @docked_bikes.pop
+  end
+
+  def dock(bike)
+    fail "Docking station full" if full?
+    @docked_bikes << bike
+  end
+
+  private
+
+  def full?
+    @docked_bikes.length >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @docked_bikes.empty?
+  end
+
+end
